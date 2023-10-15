bb56c4b62bfaf7f4494a77d85b72d37c439047d5
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-10-15 21:50:03 +0900

    add some more

--- 21.go
+++ 21.go
@@ -1,17 +1,19 @@
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
-
 package main
 
-func main() {
-	//mergeTwoLists({1, 2, 4}, {1,3,4})
+type ListNode struct {
+	Val  int
+	Next *ListNode
 }
 
-func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+//func main() {
+//	//mergeTwoLists({1, 2, 4}, {1,3,4})
+//}
 
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	dummy := list1
+
+	for {
+		for {
+		}
+	}
 }
